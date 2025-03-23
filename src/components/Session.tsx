8d5d4c00e31af51b0579b8f993d0dc714883aec3
Aryan Bisht <bishtaryan80@gmail.com>
2025-03-24 02:11:25 +0530

    removed comment

--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -277,7 +277,6 @@ const API_URL = import.meta.env.VITE_API_URL;
           </ScrollArea>
         )}
 
-        {/* Modal for Viewing Session Details */}
         {isModalOpen && selectedSession && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
             <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
